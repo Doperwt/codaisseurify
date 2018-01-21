f38478b9f568c7202536fb786c978d08794d3426
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-21 15:58:10 +0100

    adding sorting

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,10 @@
 class ArtistsController < ApplicationController
   before_action :all_artists, only: [:index]
   before_action :set_artist, only: [:show,:destroy]
-  def index ; end
+  def index
+    need_sort
+
+  end
   def show
     @song = @artist.songs.build
   end
@@ -13,10 +16,25 @@ class ArtistsController < ApplicationController
 
   private
   def all_artists
+    @search = params[:search]
     @artists = Artist.all
   end
   def set_artist
     @artist = Artist.find(params[:id])
     @songs = Song.where(artist:@artist)
+  end
+  def need_sort
+    case @search
+    when "Name Ascending"
+      @artists.order!('name ASC')
+    when "Name Descending"
+      @artists.order!('name DESC')
+    when "CA"
+      @artists.order!(created_at: :asc )
+    when "CD"
+      @artists.order!(created_at: :desc )
+    else
+      @artists.order!(name: :asc)
+    end
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,10 +3,6 @@ class Artist < ApplicationRecord
   has_one :photo, dependent: :destroy
   validates :name, presence: true, length: { maximum:50 }
   validates :bio , presence: true, length: { maximum: 500}
-  def sort_by_name
-    
-  end
-  def sort_by_created
+  
 
-  end
 end
